63d346552009432df5e7bd159d8d8e4bf1af4d74
Gianni Fiorentini <fiorentinig@gmail.com>
2023-04-18 09:57:47 +0000

    record audio and video streams and mixed into a new video

--- src/components/Recorder.jsx
+++ src/components/Recorder.jsx
@@ -5,6 +5,7 @@ import html2canvas from "html2canvas";
 class Recorder extends React.Component {
 
   constructor(props) {
+
     super(props)
     this.state = {
       videoSize: 0,
@@ -12,42 +13,67 @@ class Recorder extends React.Component {
       videoUrl: ""
     }
 
+    this.canvasRef = React.createRef();
+    this.modalRef = React.createRef();
+
     this.startRecording = this.startRecording.bind(this)
     this.stopRecording = this.stopRecording.bind(this)
     this.download = this.download.bind(this)
     this.toggleModal = this.toggleModal.bind(this)
     this.playAll = this.playAll.bind(this)
     this.stopAll = this.stopAll.bind(this)
-    this.loop = this.loop.bind(this)
 
     this.chunks = []
   }
 
   componentDidMount() {
-    this.canvas = document.getElementById('canvas')
-    this.ctx = this.canvas.getContext('2d', { willReadFrequently: true })
+    this.ctx = this.canvasRef.current.getContext('2d', { willReadFrequently: true })
     this.captureElement = document.getElementById('room')
   }
 
+  recordAudio(videoElement) {
+    const ctx = new AudioContext()
+    const source = ctx.createMediaElementSource(videoElement)
+    const stream_dest = ctx.createMediaStreamDestination()
+    source.connect(stream_dest)
+    return stream_dest.stream
+  }
+
+  recordScreen(screenElement) {
+    const frameRate = 30
+    this.canvasRef.current.width = screenElement.clientWidth
+    this.canvasRef.current.height = screenElement.clientHeight
+
+    this.recordInterval = setInterval(() => {
+      html2canvas(this.captureElement).then(screenshot => {
+        this.ctx.drawImage(screenshot, 0, 0)
+      });
+    }, frameRate)
+
+    return this.canvasRef.current.captureStream(frameRate)
+  }
+
   startRecording() {
-
-    this.canvas.width = this.captureElement.clientWidth
-    this.canvas.height = this.captureElement.clientHeight
-
-    this.recordInterval = setInterval(this.loop, 1000 / 30)
-
-    let videoStream = this.canvas.captureStream(1000 / 30)
-    this.mediaRecorder = new MediaRecorder(videoStream);
-
+    
     let _this = this
+    const video = document.querySelectorAll('#video')[0]
 
+    // get audio and video streams
+    let audioStream = this.recordAudio(video)
+    let videoStream = this.recordScreen(this.captureElement)
+
+    // mix streams video and audio
+    let mixedStream = new MediaStream([...videoStream.getTracks(), ...audioStream.getAudioTracks()])
+    this.mediaRecorder = new MediaRecorder(mixedStream);
+
+    // mediaRecorder events
     this.mediaRecorder.ondataavailable = function (e) {
       _this.chunks.push(e.data);
     }
 
     this.mediaRecorder.onstop = function (e) {
       let blob = new Blob(_this.chunks, { 'type': 'video/webm' })
-      _this.setState({ videoSize: blob.size })
+      _this.setState({ videoSize: (blob.size / 1024).toFixed(2)  })
       _this.setState({ videoUrl: URL.createObjectURL(blob) })
       _this.chunks = []
     }
@@ -66,8 +92,7 @@ class Recorder extends React.Component {
   }
 
   toggleModal() {
-    let modal = document.getElementById('modal')
-    modal.classList.toggle('open')
+    this.modalRef.current.classList.toggle('open')
   }
 
   playAll() {
@@ -92,13 +117,6 @@ class Recorder extends React.Component {
     return `${year}-${month}-${day}`
   }
 
-
-  loop() {
-    html2canvas(this.captureElement).then(screenshot => {
-      this.ctx.drawImage(screenshot, 0, 0)
-    });
-  }
-
   stopRecording() {
     clearInterval(this.recordInterval)
     this.mediaRecorder.stop()
@@ -116,18 +134,17 @@ class Recorder extends React.Component {
             <button className={`bg-cyan-200 text-cyan-950 pr-2 pl-2 m-2`} onClick={this.stopRecording}>STOP REC</button> |
             <button className={`bg-cyan-200 text-cyan-950 pr-2 pl-2 m-2`} onClick={this.toggleModal}>PREVIEW</button>
             <button className={`bg-cyan-200 text-cyan-950 pr-2 pl-2 m-2`} onClick={this.download}>DOWNLOAD</button>
-
           </div>
           <div className="p-2">
-            Video size: {this.state.videoSize / 1024} Kb
+            Video size: {this.state.videoSize} Kb
           </div>
           <div>
-            <canvas id="canvas" style={{ display: 'none' }}></canvas>
+            <canvas id="canvas" ref={this.canvasRef} style={{ display: 'none' }}></canvas>
           </div>
         </div>
-        <div id="modal" className="absolute inset-0  w-full  justify-center">
+        <div id="modal" ref={this.modalRef} className="absolute inset-0  w-full  justify-center">
           <div style={{ width: 1200 }} className="bg-black text-white p-16">
-            <video id="video" src={this.state.videoUrl} controls width="100%" />
+            <video id="videoPreview" src={this.state.videoUrl} controls width="100%" />
             <button className="bg-cyan-200 text-cyan-950 pr-2 pl-2 m-2" onClick={this.toggleModal}>CLOSE</button>
           </div>
         </div>
