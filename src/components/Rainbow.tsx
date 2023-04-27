4affecc58519c0d8d18176011777266c01da03f3
Gianni <fiorentinig@gmail.com>
2023-04-27 11:13:24 +0200

    liniting

--- src/components/Rainbow.tsx
+++ src/components/Rainbow.tsx
@@ -7,24 +7,24 @@ type RainbowState = {
 
 class Rainbow extends React.Component<RainbowProps, RainbowState> {
 
-    colors: string[] = ['red','yellow','green','blue']
+  colors: string[] = ['red', 'yellow', 'green', 'blue']
 
-    constructor(props: RainbowProps) {
-        super(props);
-        this.state = {currentColor:'grey'};
-    }
+  constructor(props: RainbowProps) {
+    super(props);
+    this.state = { currentColor: 'grey' };
+  }
 
-    componentDidMount() {
-        setInterval(() => {
-            this.setState({currentColor : this.colors[Math.floor(Math.random()*this.colors.length)]})
-        }, 500);
-    }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ currentColor: this.colors[Math.floor(Math.random() * this.colors.length)] })
+    }, 500);
+  }
 
-    render() {
-      return (
-        <div style={{backgroundColor: this.state.currentColor, height:200, width:200}}></div>
-      )
-    }
+  render() {
+    return (
+      <div style={{ backgroundColor: this.state.currentColor, height: 200, width: 200 }}></div>
+    )
+  }
 }
 
 export default Rainbow
